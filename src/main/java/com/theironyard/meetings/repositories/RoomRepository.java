fd78677030513a9637ff3f3fe96f6112f3c85739
Clinton Dreisbach <clinton@dreisbach.us>
2017-11-30 11:14:45 -0500

    Add multiple queries

--- src/main/java/com/theironyard/meetings/repositories/RoomRepository.java
+++ src/main/java/com/theironyard/meetings/repositories/RoomRepository.java
@@ -1,8 +1,14 @@
 package com.theironyard.meetings.repositories;
 
 import com.theironyard.meetings.entities.Room;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Integer> {
 
@@ -16,9 +22,16 @@ public interface RoomRepository extends JpaRepository<Room, Integer> {
 
     @PreAuthorize("hasRole('ADMIN')")
     @Override
-    void deleteAll();
+    void delete(Iterable<? extends Room> rooms);
 
     @PreAuthorize("hasRole('ADMIN')")
     @Override
-    void delete(Iterable<? extends Room> rooms);
+    void deleteAll();
+
+    @RestResource(path = "byBuilding")
+    List<Room> findAllByBuilding(@Param("building") String building, Pageable page);
+
+    @RestResource(path = "fullyEquipped")
+    @Query("SELECT r FROM Room r WHERE has_projector = true and has_teleconferencing = true")
+    List<Room> findAllFullyEquipped(Pageable page);
 }
